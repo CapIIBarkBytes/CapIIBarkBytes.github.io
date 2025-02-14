3d451d233dc92969a197841d98e82119d1053cf4
Joshua Johnston <2021johnston.joshua@gmail.com>
2025-02-14 16:58:59 -0500

    SCRUM-7 Testing connection to SD Card

---
+++ Commit2.ino
@@ -0,0 +1,24 @@
+#include "SD.h"
+#include "WiFi.h"
+
+const char* ssid = "BarkBytes-Access-Point";
+const char* password = "cap2barkbytes";
+
+
+void setup() {
+  Serial.begin(9600);
+  if(!SD.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  WiFi.softAP(ssid,password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("ESP32 IP: ");
+  Serial.println(IP);
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
