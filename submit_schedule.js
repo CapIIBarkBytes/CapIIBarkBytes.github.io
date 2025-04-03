9fbc745f7283c4115b6decf82f1f039deb58de76
Victoria Nwanze <vickyonwa@gmail.com>
2025-04-03 12:36:35 -0400

    updates endpoints

--- submit_schedule.js
+++ submit_schedule.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         
         try {
-            const response = await fetch("https://famous-smoothly-sunbeam.ngrok-free.app/schedule", {
+            const response = await fetch("https://famous-smoothly-sunbeam.ngrok-free.app/schedules", {
                 method: "POST",
                 headers: { "Content-Type": "application/json"},
                 body: JSON.stringify(requestData)
